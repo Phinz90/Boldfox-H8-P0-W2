8e597700153d32a7d8c2562e853a5391919fec21
Yosephine Mellyana <phinzlin@gmail.com>
2018-12-26 01:21:13 +0700

    Add files via upload

---
+++ proxytia.js
@@ -0,0 +1,25 @@
+function gameProxytia(nama, peran) {
+
+  var namaPeran = ['Ksatria', 'Tabib', 'Penyihir'];
+
+  if (nama === '') {
+    console.log('Nama harus diisi!')
+  } else if (nama !== '' && peran === '') {
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!')
+ 
+  } else {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama)
+
+    if (peran === 'Ksatria') {
+      console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+    } else if (peran === 'Tabib') {
+      console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.')
+    } else if (peran === 'Penyihir') {
+      console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+    } else if (peran !== namaPeran) {
+      console.log('Halo ' + nama + ', Peran yang kamu pilih tidak sesuai dengan system.')
+    }
+  }
+}
+
+gameProxytia('Alvin', 'Ksatria')
